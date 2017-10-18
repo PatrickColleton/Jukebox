4638b17c6b41a1d7918d550c694994ca173b1b14
PatrickColleton <patrickcolleton@gmail.com>
2017-10-18 17:42:11 -0400

    more concise javascript

--- js/index.js
+++ js/index.js
@@ -3,20 +3,19 @@ class Jukebox{
   constructor(){
     this.songs = [];
     this.currentSong = 0;
-    this.numSongs = 0;
   }
   nextSong(){
     this.stop();
     this.currentSong++;
-    if(this.currentSong > this.numSongs-1){
+    if(this.currentSong > this.songs.length-1){
       this.currentSong = 0;
     }
     this.play();
   }
   prevSong(){
     this.stop();
-    if(this.currentSong < 1){
-      this.currentSong = this.numSongs - 1;
+    if(this.currentSong<1){
+      this.currentSong = this.songs.length - 1;
     } else{
       this.currentSong--;
     }
@@ -35,14 +34,15 @@ class Jukebox{
   }
   addSong(song){
     this.songs.push(song);
-    this.numSongs++;
   }
 }
 
 var myJukeBox = new Jukebox();
+
 var junoreactor = new Audio('music/junoreactor.mp4');
 var nightwish = new Audio('music/nightwish.mp4');
 var gloryhammer = new Audio('music/gloryhammer.mp4');
+
 myJukeBox.addSong(junoreactor);
 myJukeBox.addSong(nightwish);
 myJukeBox.addSong(gloryhammer);
